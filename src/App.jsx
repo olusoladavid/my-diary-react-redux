71fa0246895b32ae81e5aa530408042e0563bcc3
Olusola Oguntimehin <sola.dav7@gmail.com>
2018-11-19 02:58:49 +0100

    chore(setup): setup testing framework for client
    - setup jest
    - setup enzyme
    - setup jest-enzyme environment
    
    [Finishes #161775625]

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+  <div>
+    <h1>Welcome to My Diary</h1>
+  </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => (
-  <div>
-    <h1>Welcome to My Diary</h1>
-  </div>
-);
+import App from "./App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("app"));
